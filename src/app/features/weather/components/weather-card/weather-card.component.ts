2f892352444b9b552c28053c1ca5a062e770cdce
Oleh Myronenko <mironenko.oleg@gmail.com>
2023-10-13 20:45:15 +0200

    adds form group to weather edit card

--- src/app/features/weather/components/weather-card/weather-card.component.ts
+++ src/app/features/weather/components/weather-card/weather-card.component.ts
@@ -1,13 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Weather } from '../../models/weather.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: ['./weather-card.component.scss'],
 })
-export class WeatherCardComponent {
+export class WeatherCardComponent implements OnInit {
   @Input() weatherInfo: Weather = new Weather();
 
   editModeEnabled = false;
+
+  weatherForm = new FormGroup({
+    temperature: new FormControl(0, [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+  });
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit() {
+    this.weatherForm.setValue({
+      temperature: this.weatherInfo.temperature!,
+      description: this.weatherInfo.description!,
+    });
+  }
+
+  saveWeather() {
+    if (this.weatherForm.untouched || this.weatherForm.invalid) {
+      return;
+    }
+
+    let weather = new Weather();
+    weather.id = this.weatherInfo.id;
+    weather.temperature = this.weatherForm.controls.temperature.value!;
+    weather.description = this.weatherForm.controls.description.value!;
+    weather.date = this.weatherInfo.date;
+
+    this.weatherService.update(weather).subscribe(() => {
+      this.weatherInfo.temperature = weather.temperature;
+      this.weatherInfo.description = weather.description;
+    });
+  }
 }
