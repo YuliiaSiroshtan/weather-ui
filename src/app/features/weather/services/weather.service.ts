1d321e3b2a7f7f0e11bea48f6c288c0c7cd0e549
Oleh Myronenko <mironenko.oleg@gmail.com>
2023-10-15 12:33:35 +0200

    implements add weather page

--- src/app/features/weather/services/weather.service.ts
+++ src/app/features/weather/services/weather.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {
   ApiClient,
   WeatherApiEntity,
+  WeatherApiRequest,
 } from 'src/app/core/services/api-client.service';
 import { Weather } from '../models/weather.model';
 
@@ -13,6 +14,15 @@ export class WeatherService {
 
   getAll() {
     return this.client.weathersGET();
+  }
+
+  add(weather: Weather) {
+    let weatherInfo = new WeatherApiRequest();
+    weatherInfo.temperature = weather.temperature;
+    weatherInfo.description = weather.description;
+    weatherInfo.date = weather.date;
+
+    return this.client.weathersPOST(weatherInfo);
   }
 
   update(weather: Weather) {
